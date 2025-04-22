a508b340aef881359deaf401aeb4f6c2a6a181bc
Jay Al Gallenero <jeegallenero@gmail.com>
2025-04-22 11:17:45 +0800

    Product Monitoring Project Flow

--- PresentationLayer/Views/IViews/Inventory/IProductMonitoringView.cs
+++ PresentationLayer/Views/IViews/Inventory/IProductMonitoringView.cs
@@ -5,11 +5,13 @@
         double InStock { set; }
         double LowStock { set; }
         double OutOfStock { set; }
+        double ProjectFlow { set; }
 
         event EventHandler PrintEvent;
 
         void SetInStockListBindingSource(BindingSource source);
         void SetLowStockListBindingSource(BindingSource source);
         void SetOutOfStockListBindingSource(BindingSource source);
+        void SetProjectFlowListBindingSource(BindingSource source);
     }
 }
